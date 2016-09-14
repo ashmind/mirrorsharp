37081e644dfd306574adf1685d34059a21555f81
Andrey Shchekin <ashmind@gmail.com>
2016-09-14 21:36:24 +1200

    Switched to underlying WebSocket in Owin engine.

--- MirrorSharp.Owin/Internal/Middleware.cs
+++ MirrorSharp.Owin/Internal/Middleware.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using MirrorSharp.Advanced;
@@ -23,13 +24,24 @@ namespace MirrorSharp.Owin.Internal {
             if (!environment.TryGetValue("websocket.Accept", out accept))
                 return _next(environment);
 
-            ((WebSocketAccept) accept)(null, e => {
-                var socket = new OwinWebSocket(e);
-                return Task.WhenAny(WebSocketLoopAsync(socket), socket.AbortedTask);
+            ((WebSocketAccept) accept)(null, async e => {
+                var contextKey = typeof(WebSocketContext).FullName;
+                var context = (WebSocketContext)e.GetValueOrDefault(contextKey);
+                if (context == null) {
+                    throw new NotSupportedException(
+                         $"At the moment, MirrorSharp requires Owin host to provide '{contextKey}'.\r\n" +
+                          "It's not in the specification, but it is provided by the IIS host at least. " +
+                          "After spending some time on this, I don't feel that a WebSocket wrapper for Owin " +
+                          "is worth the effort. However if you want to implement one, I will appreciate it.\r\n" +
+                          "You can find my attempt at https://gist.github.com/ashmind/40563ead5b467a243308a02d27c707ed."
+                    );
+                }
+
+                using (context.WebSocket) {
+                    await WebSocketLoopAsync(context.WebSocket).ConfigureAwait(false);
+                }
             });
             return Done;
         }
-
-
     }
 }
