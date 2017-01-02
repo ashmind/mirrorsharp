1b32aeffac4e47f67a80db42d8a209fb621cfbd0
Andrey Shchekin <ashmind@gmail.com>
2017-01-03 02:01:53 +1300

    [gh-37] Changed npm token approach.

---
+++ Publish-ToNpm.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2
+
+try {
+    Set-Content '.npmrc' "//registry.npmjs.org/:_authToken=$($env:NpmAuthToken)"
+    Get-Item *.tgz | % { npm publish $($_.FullName) }
+}
+catch {
+    Remove-Item ".npmrc"
+}
