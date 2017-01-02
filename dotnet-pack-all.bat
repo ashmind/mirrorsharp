08949b8f1c32b61c7ef274d37634d2b69dc66268
Andrey Shchekin <ashmind@gmail.com>
2017-01-03 01:35:32 +1300

    [gh-37] Initial attempt at npm publish.

--- dotnet-pack-all.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; @('MirrorSharp.Common', 'MirrorSharp.Owin') | %% { dotnet pack $_ --version-suffix=$version --output . --configuration Release }"
---
+++ pack-all.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; @('MirrorSharp.Common', 'MirrorSharp.Owin') | %% { dotnet pack $_ --version-suffix=$version --output . --configuration Release }"
+npm pack MirrorSharp.WebAssets
