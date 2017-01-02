d226a38868e9fa11d4e32484cad25bea9bf502b7
Andrey Shchekin <ashmind@gmail.com>
2017-01-02 18:47:16 +1300

    [gh-50] Build fixes.

--- dotnet-pack-all.bat
+++ dotnet-pack-all.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; @('MirrorSharp.Common', 'MirrorSharp.Owin') | %% { dotnet pack $_ --version-suffix=$version --output . }"
+powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; @('MirrorSharp.Common', 'MirrorSharp.Owin') | %% { dotnet pack $_ --version-suffix=$version --output . --configuration Release }"
