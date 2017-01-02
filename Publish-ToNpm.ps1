01a620ce940c32c3add96ae0d3360a182212f8c7
Andrey Shchekin <ashmind@gmail.com>
2017-01-03 02:22:21 +1300

    [gh-37] Suppressed re-publish errors.

--- Publish-ToNpm.ps1
+++ Publish-ToNpm.ps1
@@ -3,7 +3,17 @@ Set-StrictMode -Version 2
 
 try {
     Set-Content '.npmrc' "//registry.npmjs.org/:_authToken=$($env:NpmAuthToken)"
-    Get-Item *.tgz | % { npm publish $($_.FullName) }
+    Get-Item *.tgz | % { 
+        try {
+            npm publish $($_.FullName)
+        }
+        catch {
+            # not failing sine it's likely we tried to publish package
+            # twice with the same name. later this should be improved
+            # to actually check the error.
+            Write-Output $_
+        }
+    }
 }
 finally {
     Remove-Item ".npmrc"
