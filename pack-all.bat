7522b34877b074311ac08ea6e00dff6b8647ea84
Andrey Shchekin <ashmind@gmail.com>
2017-03-27 21:17:13 +1300

    Fixed pack-all script.

--- pack-all.bat
+++ pack-all.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; @('MirrorSharp.Common', 'MirrorSharp.Owin', 'MirrorSharp.Testing') | %% { dotnet pack $_ --version-suffix=$version --output . --configuration Release }"
-npm pack MirrorSharp.WebAssets
+powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; @('Common', 'Owin', 'Testing') | %% { dotnet pack $_ --version-suffix=$version --output . --configuration Release }"
+npm pack WebAssets
