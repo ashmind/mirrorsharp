fc71a92812916f85b41afb7e5a4b1e24b3cc8da3
Andrey Shchekin <ashmind@gmail.com>
2017-03-27 21:30:52 +1300

    Workaround for dotnet cli bug.

--- pack-all.bat
+++ pack-all.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; @('Common', 'Owin', 'Testing') | %% { dotnet pack $_ --version-suffix=$version --output . --configuration Release }"
+powershell "$version = 'pre-'+([DateTime]::Now.ToString('yyyyMMdd'))+'%1'; $output = (Resolve-Path .); @('Common', 'Owin', 'Testing') | %% { dotnet pack $_ --version-suffix=$version --output $output --configuration Release }"
 npm pack WebAssets
