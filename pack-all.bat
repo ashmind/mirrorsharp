62331d1ad5c2ff7c5de564677f533cf8f294ac56
Andrey Shchekin <ashmind@gmail.com>
2017-05-22 21:45:51 +1200

    Minor fix to pack-all.bat.

--- pack-all.bat
+++ pack-all.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell "$versionSuffix = '%1'; $output = (Resolve-Path .); @('Common', 'FSharp', 'Owin', 'Testing', 'VisualBasic') | %% { dotnet restore /p:VersionSuffix=$versionSuffix; dotnet pack $_ --version-suffix=$version --output $output --configuration Release }"
+powershell "$versionSuffix = '%1'; $output = (Resolve-Path .); @('Common', 'FSharp', 'Owin', 'Testing', 'VisualBasic') | %% { dotnet restore /p:VersionSuffix=$versionSuffix; dotnet pack $_ --version-suffix=$versionSuffix --output $output --configuration Release }"
 npm pack WebAssets
