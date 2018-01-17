4e997214f7b58a32ccd33d5c53ef054d2a660ecb
Robert Husák <robert@husak.cloud>
2018-01-17 01:20:02 +0100

    Gather Peachpie references recursively

--- Php/MirrorSharpPhpOptions.cs
+++ Php/MirrorSharpPhpOptions.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
 
@@ -8,21 +10,37 @@ namespace MirrorSharp.Php
     [PublicAPI]
     public class MirrorSharpPhpOptions {
         /// <summary>Contains the list of assembly reference paths to be used, not configurable.</summary>
-        public static readonly ImmutableArray<string> AssemblyReferencePaths;
-
-        static MirrorSharpPhpOptions() {
-            AssemblyReferencePaths = ImmutableArray.Create(
-                typeof(object).GetTypeInfo().Assembly.Location,                                         // mscorlib
-                typeof(Pchp.Core.Context).GetTypeInfo().Assembly.Location,                              // Peachpie.Runtime
-                typeof(Pchp.Library.Strings).GetTypeInfo().Assembly.Location,                           // Peachpie.Library
-                typeof(Peachpie.Library.XmlDom.XmlDom).GetTypeInfo().Assembly.Location,                 // Peachpie.Library.XmlDom
-                typeof(Peachpie.Library.Scripting.ScriptingProvider).GetTypeInfo().Assembly.Location    // Peachpie.Library.Scripting
-            );
-        }
+        public static readonly ImmutableArray<string> AssemblyReferencePaths = GatherPeachpieReferences();
 
         internal MirrorSharpPhpOptions() { }
 
         /// <summary>Whether to compile in Debug mode.</summary>
         public bool? Debug { get; set; }
+
+        private static ImmutableArray<string> GatherPeachpieReferences() {
+            var refKnownTypes = new[] {
+                typeof(object),                                         // mscorlib
+                typeof(Pchp.Core.Context),                              // Peachpie.Runtime
+                typeof(Pchp.Library.Strings),                           // Peachpie.Library
+                typeof(Peachpie.Library.XmlDom.XmlDom),                 // Peachpie.Library.XmlDom
+                typeof(Peachpie.Library.Scripting.ScriptingProvider)    // Peachpie.Library.Scripting
+            };
+
+            var list = refKnownTypes.Select(type => type.GetTypeInfo().Assembly).Distinct().ToList();
+            var set = new HashSet<Assembly>(list);
+
+            for (int i = 0; i < list.Count; i++) {
+                var assembly = list[i];
+                var refs = assembly.GetReferencedAssemblies();
+                foreach (var refname in refs) {
+                    var refassembly = Assembly.Load(refname);
+                    if (refassembly != null && set.Add(refassembly)) {
+                        list.Add(refassembly);
+                    }
+                }
+            }
+
+            return list.Select(ass => ass.Location).ToImmutableArray();
+        }
     }
 }
