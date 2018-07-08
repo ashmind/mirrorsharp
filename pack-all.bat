24cd38278a879b6e76d249a1388bf0b863b7c359
Andrey Shchekin <ashmind@gmail.com>
2018-07-08 21:23:45 +1200

    Fixed npm pack.

--- pack-all.bat
+++ pack-all.bat
@@ -1,3 +1,3 @@
 @echo off
 powershell "$versionSuffix = '%1'; $output = (Resolve-Path .); @('Common', 'FSharp', 'Owin', 'Testing', 'VisualBasic', 'Php') | %% { dotnet restore /p:VersionSuffix=$versionSuffix; dotnet pack $_ --version-suffix=$versionSuffix --output $output --configuration Release }"
-npm pack WebAssets
+npm pack ./WebAssets
