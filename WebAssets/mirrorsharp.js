50bf7baec881786cc39ef14a8298747798cfc4ed
Johannes Egger <j.egger@posteo.at>
2018-08-30 09:41:44 +0200

    Don't set CodeMirror language when not explicitely set in server options.
    Otherwise you always have to set it when sending server options.

--- WebAssets/mirrorsharp.js
+++ WebAssets/mirrorsharp.js
@@ -763,7 +763,7 @@
 
         function receiveServerOptions(value) {
             serverOptions = value;
-            if (value.language !== language) {
+            if (value.language !== undefined && value.language !== language) {
                 language = value.language;
                 cm.setOption('mode', languageModes[language]);
             }
