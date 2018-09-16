0721d03b02a6212cddcd01293e760d0b08f02718
Andrey Shchekin <ashmind@gmail.com>
2018-09-17 00:06:34 +1200

    [gh-10] Potential fix to pack-all

--- pack-all.bat
+++ pack-all.bat
@@ -1,6 +1,7 @@
 @echo off
 powershell "$versionSuffix = '%1'; $output = (Resolve-Path .); @('Common', 'FSharp', 'Owin', 'Testing', 'VisualBasic', 'Php') | %% { dotnet restore /p:VersionSuffix=$versionSuffix; dotnet pack $_ --version-suffix=$versionSuffix --output $output --configuration Release }"
 cd WebAssets
+call npm install
 call npm run build
 cd ..
 call npm pack ./WebAssets/dist
