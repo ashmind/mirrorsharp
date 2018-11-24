d59937aa452a6919c19a9b3d95902817afa058d8
Andrey Shchekin <ashmind@gmail.com>
2018-11-25 12:11:49 +1300

    Improved test failure detection.

--- #scripts/test.ps1
+++ #scripts/test.ps1
@@ -1,5 +1,14 @@
 Set-StrictMode -Version 2
 $ErrorActionPreference = 'Stop'
 
-dotnet test Tests.Roslyn2.NetCore\Tests.Roslyn2.NetCore.csproj
-dotnet test Tests.Roslyn2.Net46\Tests.Roslyn2.Net46.csproj
+$errorExitCode = 0
+@('NetCore', 'Net46') | % {
+    dotnet test "Tests.Roslyn2.$_\Tests.Roslyn2.$_.csproj"
+    if ($LastExitCode -ne 0) {
+        $errorExitCode = $LastExitCode
+    }
+}
+
+if ($errorExitCode -ne 0) {
+    throw "One of dotnet test commands exited with exit code $errorExitCode"
+}
