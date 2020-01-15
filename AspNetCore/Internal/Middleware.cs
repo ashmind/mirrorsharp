4c963d2e555946aa0d566d69c37cdf7b9b2dbf51
Fortune Ngwenya <fortunengwenya@gmail.com>
2020-01-15 03:50:03 +0200

    Allow developer to set a custom web socket url
    - Default behavior does not break current usages
    - Default behavior allows more usages whose URL ends with "/mirrorsharp"
    - When a WebSocketUrl is set, that is used
    - We are lenient with the case of the URL 
    - We are lenient with leading or following spaces

--- AspNetCore/Internal/Middleware.cs
+++ AspNetCore/Internal/Middleware.cs
@@ -8,15 +8,29 @@ using MirrorSharp.Internal;
 namespace MirrorSharp.AspNetCore.Internal {
     internal class Middleware : MiddlewareBase {
         private readonly RequestDelegate _next;
+        private readonly MirrorSharpOptions _options;
 
         public Middleware(RequestDelegate next, MirrorSharpOptions options) : base(options) {
             _next = Argument.NotNull(nameof(next), next);
+            _options = options;
         }
 
         public Task InvokeAsync(HttpContext context) {
-            if (!context.WebSockets.IsWebSocketRequest)
-                return _next(context);
 
+            if (!context.WebSockets.IsWebSocketRequest) {
+                return _next(context);
+            }
+
+            if (string.IsNullOrWhiteSpace(_options.WebSocketUrl)) {
+                if (!context.Request.Path.Value.EndsWith("/mirrorsharp", StringComparison.OrdinalIgnoreCase)) {
+                    return _next(context);
+                }
+            } else {
+                if (!context.Request.Path.Value.Equals(_options.WebSocketUrl.Trim(), StringComparison.OrdinalIgnoreCase)) {
+                    return _next(context);
+                }
+            }   
+            
             return StartWebSocketLoopAsync(context);
         }
 
