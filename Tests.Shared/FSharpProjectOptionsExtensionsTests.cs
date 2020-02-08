6f3b49a2361b176cd9863f756a982d3a40e473ed
Chet Husk <chusk3@gmail.com>
2020-02-08 11:32:46 -0600

    fix test type parameters

--- Tests.Shared/FSharpProjectOptionsExtensionsTests.cs
+++ Tests.Shared/FSharpProjectOptionsExtensionsTests.cs
@@ -104,7 +104,7 @@ namespace MirrorSharp.Tests {
                 false,
                 DateTime.MinValue,
                 null,
-                FSharpList<Tuple<Range.range, string>>.Empty,
+                FSharpList<Tuple<Range.range, string, string>>.Empty,
                 null,
                 null
             );
