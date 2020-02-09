df7f79cb767d03bbc74af0394aacf4472fd25802
Andrey Shchekin <ashmind@gmail.com>
2020-02-09 15:57:33 +1300

    Correct test project names

--- #scripts/test.ps1
+++ #scripts/test.ps1
@@ -2,8 +2,8 @@ Set-StrictMode -Version 2
 $ErrorActionPreference = 'Stop'
 
 $errorExitCode = 0
-@('NetCore', 'Net46') | % {
-    dotnet test "Tests.Roslyn2.$_\Tests.Roslyn2.$_.csproj"
+@('NetCore', 'Net46', 'RoslynLatest') | % {
+    dotnet test "Tests.$_\Tests.$_.csproj"
     if ($LastExitCode -ne 0) {
         $errorExitCode = $LastExitCode
     }
