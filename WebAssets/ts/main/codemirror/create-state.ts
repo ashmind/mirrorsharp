f30433b95523dc64c0a8ca05128d3694ab76c04d
Andrey Shchekin <ashmind@gmail.com>
2020-05-06 19:36:51 +1200

    Initial code highlighting

--- WebAssets/ts/main/codemirror/create-state.ts
+++ WebAssets/ts/main/codemirror/create-state.ts
@@ -2,13 +2,17 @@ import { EditorState } from '@codemirror/next/state';
 import { history } from '@codemirror/next/history';
 import { keymap } from '@codemirror/next/keymap';
 import { baseKeymap } from '@codemirror/next/commands';
+import { defaultHighlighter } from '@codemirror/next/highlight';
+import { csharp } from './lang-csharp';
 
 export function createState({ initialText }: { initialText?: string }) {
     return EditorState.create({
         doc: initialText,
         extensions: [
             history(),
-            keymap(baseKeymap)
+            keymap(baseKeymap),
+            csharp(),
+            defaultHighlighter
         ]
     });
 }
---
+++ WebAssets/ts/main/codemirror/lang-csharp.ts
@@ -0,0 +1,53 @@
+import { parser } from 'lezer-csharp-tokens-only';
+import { LezerSyntax } from '@codemirror/next/syntax';
+import { languageData } from '@codemirror/next/state';
+import { styleTags } from '@codemirror/next/highlight';
+
+export const csharpSyntax = new LezerSyntax(parser.withProps(
+  languageData.add({
+      Script: {
+          closeBrackets: { brackets: ['(', '[', '{', "'", '"', '`'] },
+          commentTokens: { line: '//', block: { open: '/*', close: '*/' } }
+      }
+  }),
+  styleTags({
+      //'get set async static': 'modifier',
+      Keyword: 'keyword',
+      //in of await yield void typeof delete instanceof': 'operatorKeyword',
+      //'export import let var const function class extends': 'keyword definition',
+      //'with debugger from as': 'keyword',
+      //TemplateString: 'string#2',
+      //'BooleanLiteral Super': 'atom',
+      //this: 'self',
+      //null: 'null',
+      //   VariableName: 'variableName',
+      //   VariableDefinition: 'variableName definition',
+      //   Label: 'labelName',
+      //   PropertyName: 'propertyName',
+      //   PropertyNameDefinition: 'propertyName definition',
+      //   'PostfixOp UpdateOp': 'updateOperator',
+      //   LineComment: 'lineComment',
+      //   BlockComment: 'blockComment',
+      Number: 'number',
+      String: 'string',
+      // ArithOp: 'arithmeticOperator',
+      // LogicOp: 'logicOperator',
+      // BitOp: 'bitwiseOperator',
+      // CompareOp: 'compareOperator',
+      // RegExp: 'regexp',
+      // Equals: 'operator definition',
+      // Spread: 'punctuation',
+      // 'Arrow :': 'punctuation definition',
+      //   '( )': 'paren',
+      //   '[ ]': 'squareBracket',
+      //   '{ }': 'brace',
+      //   '.': 'derefOperator',
+      //   ', ;': 'separator'
+      Punctuation: 'punctuation'
+  })
+));
+
+/// Returns an extension that installs the JavaScript syntax provider.
+export function csharp() {
+    return csharpSyntax.extension;
+}
