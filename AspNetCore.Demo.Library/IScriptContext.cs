146e50900a7d192aaff0cf117f3123f35158daf1
Phil Haack <haacked@gmail.com>
2020-08-27 08:57:04 -0700

    Move each class to a file
    And format code according to fit the mirrorsharp style.

---
+++ AspNetCore.Demo.Library/IScriptContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCore.Demo.Library {
+    public interface IScriptContext {
+        string Arguments { get; }
+
+        IReadOnlyList<string> Messages { get; }
+    }
+}
