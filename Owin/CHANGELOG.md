73cde4a75d7ec5e0fa703d74577bad765a59f01a
Andrey Shchekin <ashmind@gmail.com>
2021-03-04 23:12:37 +1300

    Ensure .NET Framework SharpLab has access to internal guards

--- Owin/CHANGELOG.md
+++ Owin/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [2.3.3] - 2020-03-04
+
+### Changed
+- Internal changes (exposed some internals to SharpLab)
+
 ## [2.3.2] - 2020-03-04
 
 ### Changed
--- Owin/Properties/AssemblyInfo.cs
+++ Owin/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -7,3 +8,5 @@
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("00cc1a50-b233-4045-9540-cb2316808c1b")]
+
+[assembly: InternalsVisibleTo("SharpLab.Server")]
