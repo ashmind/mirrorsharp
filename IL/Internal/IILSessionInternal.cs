a19f6cc11ec57b34daefe507e16ba50c84928733
Andrey Shchekin <ashmind@gmail.com>
2021-09-06 21:41:28 +1200

    Add ILanguageSessionInternal to IILSessionInternal

--- IL/Internal/IILSessionInternal.cs
+++ IL/Internal/IILSessionInternal.cs
@@ -1,8 +1,9 @@
 using System.Text;
 using MirrorSharp.IL.Advanced;
+using MirrorSharp.Internal.Abstraction;
 
 namespace MirrorSharp.IL.Internal {
-    internal interface IILSessionInternal : IILSession {
+    internal interface IILSessionInternal : IILSession, ILanguageSessionInternal {
         int TextLength { get; }
         StringBuilder GetTextBuilderForReadsOnly();
     }
