d1f7a0f86a50fd0f03547f22566e5d4c5b976439
Andrey Shchekin <ashmind@gmail.com>
2022-04-06 00:29:39 +1200

    [ashmind/SharpLab#955] Potential workaround for Roslyn main branch

--- Common/Internal/Roslyn/RoslynLanguageBase.cs
+++ Common/Internal/Roslyn/RoslynLanguageBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Composition;
 using System.Composition.Hosting;
 using System.Linq;
 using System.Reflection;
@@ -45,6 +46,9 @@ namespace MirrorSharp.Internal.Roslyn {
         }
 
         private CompositionHost CreateCompositionHost(string featuresAssemblyName, string workspacesAssemblyName) {
+            // TODO: Naive way to force-load "System.Composition.AttributedModel" -- TODO: improve/investigate
+            _ = typeof(ExportAttribute).Assembly;
+
             var types = new[] {
                 RoslynAssemblies.MicrosoftCodeAnalysisWorkspaces,
                 RoslynAssemblies.MicrosoftCodeAnalysisFeatures,
