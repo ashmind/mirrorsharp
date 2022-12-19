2652d9e89a78cda62d66b9616ff7a81f6d215ba4
Rustam Sayfutdinov <rstm.sf@gmail.com>
2022-12-19 22:42:23 +0300

    Remove GenerateReferenceAssembliesPathTask

--- Tests/MirrorSharpOptionsWithXmlDocumentation.cs
+++ Tests/MirrorSharpOptionsWithXmlDocumentation.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Immutable;
+using System.IO;
 using Microsoft.CodeAnalysis;
 
 namespace MirrorSharp.Tests {
     public static partial class MirrorSharpOptionsWithXmlDocumentation {
-        #if NETCOREAPP
+
         private static readonly string MscorlibReferenceAssemblyPath =
-            AppDomain.CurrentDomain.BaseDirectory
-                + @"\ref-assemblies\System.Runtime.dll";
-        #endif
+#if NETCOREAPP
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory!, "ref-assemblies", "System.Runtime.dll");
+#else
+            File
+                .ReadAllText(Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory!, "PkgMicrosoft_NETFramework_ReferenceAssemblies.txt"))
+                .TrimEnd('\r', '\n');
+#endif
 
         public static MirrorSharpOptions Instance { get; } = new MirrorSharpOptions().SetupCSharp(c => {
             c.MetadataReferences = ImmutableList<MetadataReference>.Empty;
